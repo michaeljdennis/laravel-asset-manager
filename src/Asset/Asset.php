94219c2881138ac26ef2cea63dd197f0cf8ad223
Michael Dennis <michaeljdennis@gmail.com>
2013-11-06 21:16:11 -0800

    Remove Laravel specific code

--- src/Asset/Asset.php
+++ src/Asset/Asset.php
@@ -1,32 +1,30 @@
 <?php namespace Asset;
 
-class Asset
-{
+class Asset {
 	private static $cssFiles = array();	
 	private static $jsFiles = array();
 
-	public static function add($source)
-	{
+	public static function add($source) {
 		if(pathinfo($source, PATHINFO_EXTENSION) == 'css') {
 			array_push(self::$cssFiles, $source);
 		} else if(pathinfo($source, PATHINFO_EXTENSION) == 'js') {
 			array_push(self::$jsFiles, $source);
-		} else {
-			Log::error('Incompatible asset extension: ' . $source);
 		}
 	}
 
-	public static function css()
-	{
+	public static function css() {
 		foreach(self::$cssFiles as $cssFile) {
-			echo HTML::style($cssFile);
+			echo <<<HTML
+<link media="all" type="text/css" rel="stylesheet" href="$cssFile">
+HTML;
 		}
 	}
 
-	public static function js()
-	{
+	public static function js() {
 		foreach(self::$jsFiles as $jsFile) {
-			echo HTML::script($jsFile);
+			echo <<<HTML
+<script src="$jsFile"></script>
+HTML;
 		}
 	}
 }
